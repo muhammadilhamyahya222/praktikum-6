dbdd23e3fb2707cc9fb9786bb3d65be5fddfdef4
muhammadilhamyahya222 <62550125+muhammadilhamyahya222@users.noreply.github.com>
2020-12-08 21:16:10 +0700

    second commit

--- user.js
+++ user.js
@@ -5,7 +5,8 @@ const cors = require("cors")
 const mysql = require("mysql")
 const md5 = require("md5")
 const Cryptr = require("cryptr")
-const crypt = new Cryptr("140533601726") 
+const crypt = new Cryptr("140533601726") // secret key, boleh diganti kok
+
 
 // implementation
 const app = express()
@@ -29,6 +30,42 @@ db.connect(error => {
     }
 })
 
+
+// endpoint login user (authentication)
+app.post("/user/auth", (req, res) => {
+    // tampung username dan password
+    let param = [
+        req.body.username, //username
+        md5(req.body.password) // password
+    ]
+    
+
+    // create sql query
+    let sql = "select * from user where username = ? and password = ?"
+
+    // run query
+    db.query(sql, param, (error, result) => {
+        if (error) throw error
+
+        // cek jumlah data hasil query
+        if (result.length > 0) {
+            // user tersedia
+            res.json({
+                message: "Logged",
+                token: crypt.encrypt(result[0].id_user), // generate token
+                data: result
+            })
+        } else {
+            // user tidak tersedia
+            res.json({
+                message: "Invalid username/password"
+            })
+        }
+    })
+})
+
+
+// end-point akses data siswa
 app.get("/user", (req, res) => {
     // create sql query
     let sql = "select * from user"
@@ -49,8 +86,8 @@ app.get("/user", (req, res) => {
         res.json(response) // send response
     })
 })
-
-app.get("/user/:id", (req, res) => {
+// end-point akses data siswa berdasarkan id_siswa tertentu
+app.get("/user/:id",validateToken(), (req, res) => {
     let data = {
         id_user: req.params.id
     }
@@ -73,15 +110,14 @@ app.get("/user/:id", (req, res) => {
         res.json(response) // send response
     })
 })
-
+// end-point menyimpan data siswa
 app.post("/user", (req,res) => {
 
     // prepare data
     let data = {
         nama_user: req.body.nama_user,
         username: req.body.username,
-        password: req.body.password,
-        role: req.body.role
+        password: req.body.password
     }
 
     // create sql query insert
@@ -102,17 +138,17 @@ app.post("/user", (req,res) => {
         res.json(response) // send response
     })
 })
-
+// end-point mengubah data siswa
 app.put("/user", (req,res) => {
 
     // prepare data
     let data = [
         // data
         {
+            
             nama_user: req.body.nama_user,
             username: req.body.username,
-            password: req.body.password,
-            role: req.body.role
+            password: req.body.password
         },
 
         // parameter (primary key)
@@ -139,7 +175,7 @@ app.put("/user", (req,res) => {
         res.json(response) // send response
     })
 })
-
+// end-point menghapus data siswa berdasarkan id_siswa
 app.delete("/user/:id", (req,res) => {
     // prepare data
     let data = {
@@ -165,7 +201,48 @@ app.delete("/user/:id", (req,res) => {
     })
 })
 
+validateToken = () => {
+    return (req, res, next) => {
+        // cek keberadaan "Token" pada request header
+        if (!req.get("Token")) {
+            // jika "Token" tidak ada
+            res.json({
+                message: "Access Forbidden"
+            })
+        } else {
+            // tampung nilai Token
+            let token  = req.get("Token")
+            
+            // decrypt token menjadi id_user
+            let decryptToken = crypt.decrypt(token)
+
+            // sql cek id_user
+            let sql = "select * from user where ?"
+
+            // set parameter
+            let param = { id_user: decryptToken}
+
+            // run query
+            db.query(sql, param, (error, result) => {
+                if (error) throw error
+                 // cek keberadaan id_user
+                if (result.length > 0) {
+                    // id_user tersedia
+                    next()
+                } else {
+                    // jika user tidak tersedia
+                    res.json({
+                        message: "Invalid Token"
+                    })
+                }
+            })
+        }
+
+    }
+}
+
+
+
 app.listen(8000, () => {
     console.log("Run on port 8000")
 })
-
